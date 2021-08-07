d186dcf67b84a85560ea533ee45e8b827545345d
Nelson Wong <nelsonwong107@gmail.com>
2021-08-06 21:35:02 -0400

    Add project files.

---
+++ Prices.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+* Project		: Apartment Prices
+* Title			: Prices.cpp
+* Author		: Nelson Wong
+* Description	: Program to calculate the total cost of owning an apartment with inputs for apartment base price, number of stories taken as integers, doubles for the tax rates and 
+*/
+
+
+using namespace std;
+
+int main(void)
+{
+	return 0;
+}
